40f9849a4dbc5b0a9dc3b14be9c36f2c07b7da07
Kasper G. Mikkelsen <61277434+Kamik18@users.noreply.github.com>
2021-09-09 16:11:44 +0200

    Working system

--- Turtle/src/Modules/Motor.hpp
+++ Turtle/src/Modules/Motor.hpp
@@ -8,41 +8,24 @@ namespace motor
         Forward = 1
     };
 
-    struct IRQ_struct
-    {
-        void (*irq_func)();
-        uint8_t _pin_irq_a;
-        uint8_t _pin_irq_b;
-    };
-
     class Motor
     {
     public:
-        Motor(const uint8_t pin_pwm, const uint8_t pin_dir,
-              //const uint8_t pin_irq_a, const uint8_t pin_irq_b,
-              struct IRQ_struct *irq)
-            : _pin_pwm(pin_pwm), _pin_dir(pin_dir), _irq(irq)
+        Motor(const uint8_t pin_pwm, const uint8_t pin_dir)
+            : _pin_pwm(pin_pwm), _pin_dir(pin_dir)
         {
-
-            //_irq->_pin_irq_a = pin_irq_a;
-            //_irq->_pin_irq_b = pin_irq_b;
-
             pinMode(_pin_pwm, OUTPUT);
             pinMode(_pin_dir, OUTPUT);
-            pinMode(_irq->_pin_irq_a, INPUT);
-            pinMode(_irq->_pin_irq_b, INPUT);
         }
 
-        void runPWM(const uint32_t pwm, const direction dir)
+        void runPWM(const uint32_t pwm, const direction dir) const
         {
             analogWrite(_pin_pwm, pwm);
             digitalWrite(_pin_dir, static_cast<uint8_t>(dir));
         }
 
-        struct IRQ_struct *_irq;
-
     private:
-        uint8_t _pin_pwm;
-        uint8_t _pin_dir;
+        const uint8_t _pin_pwm;
+        const uint8_t _pin_dir;
     };
 }
--- Turtle/src/main.cpp
+++ Turtle/src/main.cpp
@@ -1,90 +1,21 @@
 #include <Arduino.h>
-//#include <EEPROM.h>
-
-//#include <MotorWheel.h>
-//#include <R2WD.h>
-
-//#include <fuzzy_table.h>
-//#include <PID_Beta6.h>
-
-//#include <SONAR.h>
+#include <Wire.h>
 #include "Modules/Motor.hpp"
 
-// Sensor positions
-//         - - - - - - - - - - - - - - - -
-//        /       power switch            \
-//       /                                 \
-//      |                                   |
-//      |                                   |
-//      |  wheel2                    wheel1 |
-//      |                                   |
-//      |                                   |
-//       \                                  /
-//        \  sonar3     sonar 2    sonar 1 /
-//         \                              /
-//          \                            /
-//            -  -  -  -  -  -  -  -  -
-//          bumper_R    bumper_C    bumper_L
-
-/******************************************/
-/*
-// SONAR
-SONAR sonar11(0x11),sonar12(0x12),sonar13(0x13);
-
-unsigned short distBuf[3];
-void sonarsUpdate() {
-    static unsigned char sonarCurr=1;
-    if(sonarCurr==3) sonarCurr=1;
-    else ++sonarCurr;
-    if(sonarCurr==1) {        
-        distBuf[1]=sonar12.getDist();        
-        sonar12.trigger();        
-    } else if(sonarCurr==2) {
-        distBuf[2]=sonar13.getDist();
-        sonar13.trigger();
-    } else {
-        distBuf[0]=sonar11.getDist();
-        sonar11.trigger();
-    }
-}
-*/
-
-/*********************************************/
-// Optional device
-// Infrared Sensor
-/*
-const uint8_t irL0_pin=0;    // Analog
-const uint8_t irC0_pin=1;
-const uint8_t irR0_pin=2;
-
-int ir_distance(unsigned char ir) {
-	int val=analogRead(ir);
-	return (6762/(val-9))-4;
-}
-*/
-
+// Pinout
 // Bumper
-uint8_t bumper_left = 1;
-uint8_t bumper_center = 2;
-uint8_t bumper_right = 3;
+const uint8_t bumper_left = 0;
+const uint8_t bumper_center = 1;
+const uint8_t bumper_right = 2;
 
-motor::IRQ_struct irq_left;
-motor::IRQ_struct irq_right;
-// motor::Motor wheel_left(9, 8, /*4, 5,*/ &irq_left);
-// motor::Motor wheel_right(10, 11, /*6, 7,*/ &irq_right);
-motor::Motor wheel_left(3, 12, /*4, 5,*/ &irq_left);
-motor::Motor wheel_right(11, 13, /*6, 7,*/ &irq_right);
+// Horn
+const uint8_t horn = 10;
 
-uint8_t horn = 0;
+// Motor instances
+const motor::Motor wheel_left(3, 12);
+const motor::Motor wheel_right(11, 13);
 
-void stop_wheels()
-{
-  wheel_left.runPWM(0, motor::direction::Forward);
-  wheel_right.runPWM(0, motor::direction::Forward);
-  delay(250);
-}
-
-void is_bumper_pressed()
+bool is_bumper_pressed()
 {
   bool is_left = digitalRead(bumper_left);
   bool is_center = digitalRead(bumper_center);
@@ -92,12 +23,49 @@ void is_bumper_pressed()
 
   bool error_bip = (!is_left || !is_center || !is_right) ? true : false;
 
-  digitalWrite(horn, error_bip);
+  if (!is_left || !is_center || !is_right)
+  {
+    wheel_left.runPWM(0, motor::direction::Forward);
+    wheel_right.runPWM(0, motor::direction::Forward);
 
-  Serial.println("Left: " + String(is_left) + "\t\t" +
-                 "Center: " + String(is_center) + "\t" +
-                 "Right: " + String(is_right) + "\t\t" +
-                 "Beep: " + String(error_bip));
+    // Indicate an error
+    digitalWrite(horn, HIGH);
+    return true;
+  }
+  else
+  {
+    digitalWrite(horn, LOW);
+    return false;
+  }
+}
+
+void receiveEvent(int howMany)
+{
+  // Dummy read
+  Wire.read();
+
+  // Read the 4 incoming bytes
+  // order: [left_pwm, left_dir, left_dir, left_pwm]
+  if ((Wire.available() == 4) && (!is_bumper_pressed()))
+  {
+    wheel_left.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
+    wheel_right.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
+  }
+  else
+  {
+    // Clear buffer
+    while (Wire.available())
+    {
+      // Read dummy
+      Wire.read();
+    }
+  }
+}
+
+void requestEvent()
+{
+  // Return if bumper is pressed
+  Wire.write(is_bumper_pressed());
 }
 
 void setup()
@@ -119,22 +87,19 @@ void setup()
   // Initialize wheels
   wheel_left.runPWM(0, motor::direction::Forward);
   wheel_right.runPWM(0, motor::direction::Forward);
+
+  // Configurate I2C
+  Wire.begin(25);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
 }
 
 const uint16_t delay_msec = 500;
 const uint8_t left = 0;
 const uint8_t right = 0;
-uint8_t counter = 0;
+uint8_t counter = 25;
 void loop()
 {
-  wheel_left.runPWM(left + counter, motor::direction::Forward);
-  wheel_right.runPWM(right + counter, motor::direction::Forward);
-  delay(delay_msec);
-  stop_wheels();
-  delay(delay_msec);
-  Serial.println(counter);
-  counter++;
-
   is_bumper_pressed();
-  delay(100);
+  delay(10);
 }
