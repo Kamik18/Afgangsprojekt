373a716289d1fb7d583c3bef6f7d3f85004c46c4
Kasper G Mikkelsen <kaspercom2008@hotmail.com>
2021-09-12 10:22:38 +0200

    Update

---
+++ Turtle/src/Watchdog/Watchdog.cpp
@@ -0,0 +1,90 @@
+//===================================================================
+// File: Watchdog.cpp
+//
+// Description:
+//  Module to encapsulation of watchdog functionality.
+//===================================================================
+//-------------------------------------------------------------------
+// Includes
+//-------------------------------------------------------------------
+#include "Watchdog.hpp"
+#include "stm32wbxx_ll_iwdg.h"
+#include "stm32wbxx_ll_rcc.h"
+
+using iotsensor_module_watchdog::Watchdog;
+
+namespace iotsensor_module_watchdog {
+    //-----------------------------
+    // @brief Starts the watchdog.
+    // @param timeout - The timeout for the watchdog.
+    void Watchdog::begin(const uint32_t timeout) const {
+        if (is_wdg_timeout(timeout)) {
+            // Enable the IWDG by writing 0x0000 CCCC in the IWDG_KR register
+            ::LL_IWDG_Enable(IWDG);
+            // Set timeout
+            this->set(timeout);
+        }
+    }
+
+    //-----------------------------
+    // @brief Set the timeout.
+    // @param timeout - The timeout for the watchdog.
+    void Watchdog::set(const uint32_t timeout) const {
+        // Compute the prescaler value
+        uint8_t        prescaler = 0;
+        uint16_t       divider   = 0;
+        const uint16_t bitshift  = 4;
+
+        // Convert timeout to seconds
+        const uint32_t second = 1'000'000;
+        const auto     t_sec  = static_cast<float_t>((timeout / second) * LSI_VALUE);
+
+        do {
+            divider = static_cast<uint16_t>(bitshift << prescaler);
+            ++prescaler;
+        } while ((t_sec / divider) > IWDG_RLR_RL);
+
+        // Validate the prescaler
+        --prescaler;
+        if ((prescaler <= LL_IWDG_PRESCALER_256) && (is_wdg_timeout(timeout))) {
+            const auto reload = static_cast<uint32_t>((t_sec / divider) - 1);
+
+            // Enable register access by writing 0x0000 5555 in the IWDG_KR register
+            ::LL_IWDG_EnableWriteAccess(IWDG);
+            // Write the IWDG prescaler by programming IWDG_PR from 0 to 7
+            // LL_IWDG_PRESCALER_4 (0) is lowest divider
+            ::LL_IWDG_SetPrescaler(IWDG, static_cast<uint32_t>(prescaler));
+            // Write the reload register (IWDG_RLR)
+            ::LL_IWDG_SetReloadCounter(IWDG, reload);
+
+            // Update registers
+            while (::LL_IWDG_IsReady(IWDG) != 1) {
+                // Wait for the registers to be updated (IWDG_SR = 0x0000 0000)
+            }
+
+            // Refresh the counter value with IWDG_RLR (IWDG_KR = 0x0000 AAAA)
+            ::LL_IWDG_ReloadCounter(IWDG);
+        }
+    }
+
+    //-----------------------------
+    // @brief Reload the watchdog.
+    void Watchdog::reload() const {
+        // Refresh the counter value with IWDG_RLR (IWDG_KR = 0x0000 AAAA)
+        ::LL_IWDG_ReloadCounter(IWDG);
+    }
+
+    //-----------------------------
+    // @brief Validate the watchdog timeout.
+    // @param timeout - The timeout for the watchdog.
+    // @return bool - True if the timeout is accepted.
+    bool Watchdog::is_wdg_timeout(const uint32_t timeout) const {
+        // Minimal timeout in microseconds
+        const uint32_t min_timeout = 125;
+        // Maximal timeout in microseconds
+        const uint32_t max_timeout = (8000 * IWDG_RLR_RL);
+
+        // Return if the timeout is valid
+        return ((timeout >= min_timeout) && (timeout <= max_timeout));
+    }
+} // namespace iotsensor_module_watchdog
--- Turtle/src/main.cpp
+++ Turtle/src/main.cpp
@@ -1,12 +1,12 @@
+#include "Modules/Motor.hpp"
 #include <Arduino.h>
 #include <Wire.h>
-#include "Modules/Motor.hpp"
 
 // Pinout
 // Bumper
-const uint8_t bumper_left = 0;
+const uint8_t bumper_left   = 0;
 const uint8_t bumper_center = 1;
-const uint8_t bumper_right = 2;
+const uint8_t bumper_right  = 2;
 
 // Horn
 const uint8_t horn = 10;
@@ -15,91 +15,87 @@ const uint8_t horn = 10;
 const motor::Motor wheel_left(3, 12);
 const motor::Motor wheel_right(11, 13);
 
-bool is_bumper_pressed()
-{
-  bool is_left = digitalRead(bumper_left);
-  bool is_center = digitalRead(bumper_center);
-  bool is_right = digitalRead(bumper_right);
+bool is_bumper_pressed() {
+    bool is_left   = digitalRead(bumper_left);
+    bool is_center = digitalRead(bumper_center);
+    bool is_right  = digitalRead(bumper_right);
 
-  bool error_bip = (!is_left || !is_center || !is_right) ? true : false;
+    bool error_bip = (!is_left || !is_center || !is_right) ? true : false;
 
-  if (!is_left || !is_center || !is_right)
-  {
+    if (!is_left || !is_center || !is_right) {
+        wheel_left.runPWM(0, motor::direction::Forward);
+        wheel_right.runPWM(0, motor::direction::Forward);
+
+        // Indicate an error
+        digitalWrite(horn, HIGH);
+        return true;
+    } else {
+        digitalWrite(horn, LOW);
+        return false;
+    }
+}
+
+void receiveEvent(int howMany) {
+    // Dummy read
+    Wire.read();
+
+    // Read the 4 incoming bytes
+    // order: [left_pwm, left_dir, left_dir, left_pwm]
+    if ((Wire.available() == 4) && (!is_bumper_pressed())) {
+        wheel_left.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
+        wheel_right.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
+    } else {
+        // Clear buffer
+        while (Wire.available()) {
+            // Read dummy
+            Wire.read();
+        }
+    }
+}
+
+void requestEvent() {
+    // Return if bumper is pressed
+    Wire.write(is_bumper_pressed());
+}
+
+void setup() {
+    // Begin serial
+    Serial.begin(115200);
+
+    // Set bumper values
+    pinMode(bumper_left, INPUT);
+    pinMode(bumper_center, INPUT);
+    pinMode(bumper_right, INPUT);
+    pinMode(horn, OUTPUT);
+    digitalWrite(horn, LOW);
+
+    // Configurate wheel PWM
+    analogWriteFrequency(31250);
+    analogWriteResolution(8);
+
+    // Initialize wheels
     wheel_left.runPWM(0, motor::direction::Forward);
     wheel_right.runPWM(0, motor::direction::Forward);
 
-    // Indicate an error
-    digitalWrite(horn, HIGH);
-    return true;
-  }
-  else
-  {
-    digitalWrite(horn, LOW);
-    return false;
-  }
-}
+    // Configurate I2C
+    Wire.begin(25);
+    Wire.onRequest(requestEvent);
+    Wire.onReceive(receiveEvent);
 
-void receiveEvent(int howMany)
-{
-  // Dummy read
-  Wire.read();
-
-  // Read the 4 incoming bytes
-  // order: [left_pwm, left_dir, left_dir, left_pwm]
-  if ((Wire.available() == 4) && (!is_bumper_pressed()))
-  {
-    wheel_left.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
-    wheel_right.runPWM(Wire.read(), static_cast<motor::direction>(Wire.read()));
-  }
-  else
-  {
-    // Clear buffer
-    while (Wire.available())
-    {
-      // Read dummy
-      Wire.read();
-    }
-  }
-}
-
-void requestEvent()
-{
-  // Return if bumper is pressed
-  Wire.write(is_bumper_pressed());
-}
-
-void setup()
-{
-  // Begin serial
-  Serial.begin(115200);
-
-  // Set bumper values
-  pinMode(bumper_left, INPUT);
-  pinMode(bumper_center, INPUT);
-  pinMode(bumper_right, INPUT);
-  pinMode(horn, OUTPUT);
-  digitalWrite(horn, LOW);
-
-  // Configurate wheel PWM
-  analogWriteFrequency(31250);
-  analogWriteResolution(8);
-
-  // Initialize wheels
-  wheel_left.runPWM(0, motor::direction::Forward);
-  wheel_right.runPWM(0, motor::direction::Forward);
-
-  // Configurate I2C
-  Wire.begin(25);
-  Wire.onRequest(requestEvent);
-  Wire.onReceive(receiveEvent);
+    Serial.println("Test script");
+    // Test script
+    wheel_left.runPWM(25, motor::direction::Forward);
+    wheel_right.runPWM(125, motor::direction::Reverse);
+    delay(2000);
+    wheel_left.runPWM(0, motor::direction::Forward);
+    wheel_right.runPWM(0, motor::direction::Reverse);
 }
 
 const uint16_t delay_msec = 500;
-const uint8_t left = 0;
-const uint8_t right = 0;
-uint8_t counter = 25;
-void loop()
-{
-  is_bumper_pressed();
-  delay(10);
+const uint8_t  left       = 0;
+const uint8_t  right      = 0;
+uint8_t        counter    = 25;
+void           loop() {
+    is_bumper_pressed();
+    delay(10);
 }
