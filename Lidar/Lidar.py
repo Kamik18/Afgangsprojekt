7ef64299df8f5baeab5b25d8c33059437759f947
Kasper G. Mikkelsen <61277434+Kamik18@users.noreply.github.com>
2021-09-16 11:27:43 +0200

    Update

--- Lidar/Lidar.py
+++ Lidar/Lidar.py
@@ -1,8 +1,29 @@
 import os
-
-print("Directory:")
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 data_file = open(os.getcwd() + "\Lidar\data.txt")
 data = data_file.readline()
-print(data)
 data_file.close()
+
+# Remove brackets
+for character in "{ }":
+    data = data.replace(character, '')
+# Slit the string into a list of points
+data_list = data.split(',', data.count(','))
+
+# Create arrays for x and y
+data_x = []
+data_y = []
+for x in data_list:
+    compoents = x.split(':', 1)
+    angle = -math.radians(float(compoents[0]))
+    lenght = float(compoents[1])
+    if (lenght > 10):
+        data_x.append(lenght * np.cos(angle))   
+        data_y.append(lenght * np.sin(angle))
+
+#plt.plot(data_x, data_y)
+plt.plot(data_x, data_y, '.')
+plt.show()
