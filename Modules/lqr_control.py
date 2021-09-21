def23d912eec92844d7d3bfbbcd59395275e1900
Kasper G. Mikkelsen <61277434+Kamik18@users.noreply.github.com>
2021-09-21 15:52:24 +0200

    Update

--- Modules/lqr_control.py
+++ Modules/lqr_control.py
@@ -7,81 +7,76 @@ the state (i.e. x position, y position, yaw angle) at each timestep
 import numpy as np
 import matplotlib.pyplot as plt
 from Modules.kinematics import *
- 
-show_animation = True
- 
-def closed_loop_prediction(desired_traj): 
-    ## Simulation Parameters
-    T = desired_traj.shape[0]  # Maximum simulation time
-    goal_dis = 0.1 # How close we need to get to the goal
-    goal = desired_traj[-1,:] # Coordinates of the goal
-    dt = 0.1 # Timestep interval
-    time = 0.0 # Starting time
 
-    ## Initial States 
-    state = np.array([desired_traj[0,0], desired_traj[0,1], 0]) # Initial state of the car
-    state_est = state.copy()
- 
-    ## Get the Cost-to-go and input cost matrices for LQR
-    Q = get_Q() # Defined in kinematics.py
-    R = get_R() # Defined in kinematics.py
- 
- 
-    ## Initialize the Car and the Car's landmark sensor 
+show_animation = True
+
+
+def closed_loop_prediction(desired_traj):
+    # Simulation Parameters
+    T = desired_traj.shape[0]  # Maximum simulation time
+    goal_dis = 0.01  # How close we need to get to the goal
+    goal = desired_traj[-1, :]  # Coordinates of the goal
+    dt = 0.1  # Timestep interval
+    time = 0.0  # Starting time
+
+    # Initial States
+    # Initial state of the car
+    state = np.array([desired_traj[0, 0], desired_traj[0, 1], 0])
+
+    # Get the Cost-to-go and input cost matrices for LQR
+    Q = get_Q()  # Defined in kinematics.py
+    R = get_R()  # Defined in kinematics.py
+
+    # Initialize the Car and the Car's landmark sensor
     DiffDrive = DifferentialDrive()
-         
+
     # Process noise and sensor measurement noise
     V = DiffDrive.get_V()
- 
-    ## Create objects for storing states and estimated state
+
+    # Create objects for storing states and estimated state
     t = [time]
     traj = np.array([state])
-    traj_est = np.array([state_est])
- 
-    ind = 0
-    while T >= time:         
-        ## Point to track
-        ind = int(np.floor(time))
-        goal_i = desired_traj[ind,:]
- 
-        ## Generate noise
-        # v = process noise, w = measurement noise
-        v = np.random.multivariate_normal(np.zeros(V.shape[0]),V)
- 
-        ## Generate optimal control commands
-        u_lqr = dLQR(DiffDrive,Q,R,state_est,goal_i[0:3],dt)
- 
-        ## Move forwad in time
-        state = DiffDrive.forward(state,u_lqr,v,dt)
-                 
-        # Add sensors and update position        
-        state_est = state
 
-        # Increment time
-        time = time + dt
- 
+    ind = 0
+    while T >= time:
+        # Point to track
+        ind = int(np.floor(time))
+
+        goal_i = desired_traj[ind, :]
+
+        # Generate optimal control commands
+        u_lqr = dLQR(DiffDrive, Q, R, state, goal_i[0:3], dt)
+        
+        # Set pwm
+
+        # Add sensors and update position
+        # Move forwad in time
+        state = DiffDrive.forward(state, u_lqr, dt)
+
         # Store the trajectory and estimated trajectory
         t.append(time)
-        traj = np.concatenate((traj,[state]),axis=0)
-        traj_est = np.concatenate((traj_est,[state_est]),axis=0)
- 
+        traj = np.concatenate((traj, [state]), axis=0)
+
         # Check to see if the robot reached goal
         if np.linalg.norm(state[0:2]-goal[0:2]) <= goal_dis:
             print("Goal reached")
             break
- 
-        ## Plot the vehicles trajectory
+
+        if np.linalg.norm(state[0:2]-goal_i[0:2]) <= 0.1:
+            # Increment time
+            time = time + dt
+
+        # Plot the vehicles trajectory
         if time % 1 < 0.1 and show_animation:
             plt.cla()
-            plt.plot(desired_traj[:,0], desired_traj[:,1], "-r", label="course")
-            plt.plot(traj[:,0], traj[:,1], "ob", label="trajectory")
-            plt.plot(traj_est[:,0], traj_est[:,1], "sk", label="estimated trajectory")
- 
-            plt.plot(goal_i[0], goal_i[1], "xg", label="target")
+            plt.plot(desired_traj[:, 0],
+                     desired_traj[:, 1], "-r", label="course")
+            plt.plot(traj[:, 0], traj[:, 1], "ob", label="trajectory")
+            plt.legend()
             plt.axis("equal")
             plt.grid(True)
             plt.title("speed[m/s]:" + str(round(np.mean(u_lqr), 2)) +
                       ",target index:" + str(ind))
             plt.pause(0.0001)
- 
+
     return t, traj
--- __main__.py
+++ __main__.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     print("LQR steering control tracking start")
  
     # Create the track waypoints
-    ax = [8.3, 8.0, 7.2, 6.2, 6.5, 1.5,-2.0,-3.5, 2, 10]
-    ay = [0.7, 4.3, 4.5, 4.0, 0.7, 1.3, 3.3, 1.5, 2, 10]
+    ax = [0, 0.1, 5]
+    ay = [1, 1 , 0]
     
     # Compute the desired trajectory
     desired_traj = compute_traj(ax,ay)
@@ -14,17 +14,14 @@ if __name__ == '__main__':
     t, trajectory = closed_loop_prediction(desired_traj)
  
     # Display the trajectory that the mobile robot executed
-    if show_animation:
-        plt.close()
-        flg, _ = plt.subplots(1)
-        plt.plot(ax, ay, "xb", label="Inputs")
-        plt.plot(desired_traj[:,0], desired_traj[:,1], "-r", label="Trajectory")
-        plt.plot(trajectory[:,0], trajectory[:,1], "-g", label="Tracking")
-        plt.grid(True)
-        plt.axis("equal")
-        plt.xlabel("x[m]")
-        plt.ylabel("y[m]")
-        plt.legend()
- 
-        plt.show()
+    plt.close()
+    flg, _ = plt.subplots(1)
+    plt.plot(desired_traj[:,0], desired_traj[:,1], "-r", label="Trajectory")
+    plt.plot(trajectory[:,0], trajectory[:,1], "-g", label="Tracking")
+    plt.grid(True)
+    plt.axis("equal")
+    plt.xlabel("x[m]")
+    plt.ylabel("y[m]")
+    plt.legend()
+    plt.show()
     
