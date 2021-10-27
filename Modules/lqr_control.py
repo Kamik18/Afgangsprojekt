896eef5692d4b3e4fcf27db893d95e23c256655a
Kasper G Mikkelsen <kaspercom2008@hotmail.com>
2021-10-27 16:18:09 +0200

    Update

--- Modules/lqr_control.py
+++ Modules/lqr_control.py
@@ -10,12 +10,11 @@ def get_R():
 
 
 def get_Q():
-    penalty_pos = 0.3
+    penalty_pos = 0.5
     penalty_ang = 1.0
-    return np.array([[penalty_pos, 0, 0],  # Penalize X position error (global coordinates)
-                     # Penalize Y position error (global coordinates)
-                     [0, penalty_pos, 0],
-                     [0, 0, penalty_ang]])  # Penalize heading error (global coordinates)
+    return np.array([[penalty_pos, 0, 0],  # X
+                     [0, penalty_pos, 0],  # Y
+                     [0, 0, penalty_ang]]) # Theta
 
 
 def validate(value, limit_lower, limit_upper):
@@ -54,61 +53,65 @@ def closed_loop_prediction(desired_traj, simulation=False):
     # Create objects for storing states and estimated state
     trajectory = np.array([state])
 
-    prev_distance = np.inf
-    index = 1
+    try:
+        prev_distance = np.inf
+        index = 1
 
-    while (index < len(desired_traj)):
-        # Generate optimal control commands
-        u_lqr = kin.dLQR(Q, R, state, desired_traj[index, 0:3], dt)
+        while (index < len(desired_traj)):
+            # Generate optimal control commands
+            u_lqr = kin.dLQR(Q, R, state, desired_traj[index, 0:3], dt)
 
-        if (index < (len(desired_traj) - 0)):
-            factor = VELOCITY / (abs(u_lqr[0]) + abs(u_lqr[1]))
-            u_lqr *= validate(factor, 0, np.inf)
+            if (index < (len(desired_traj) - 0)):
+                factor = VELOCITY / (abs(u_lqr[0]) + abs(u_lqr[1]))
+                u_lqr *= validate(factor, 0, np.inf)
 
-        # Add sensors and update position
-        # Move forwad in time
-        if (simulation):
-            state = kin.forward(state, u_lqr, dt)
-        else:
-            turtle.set_velocity(u_lqr[0], u_lqr[1])
-            state, bumper = turtle.get_pos(OFFSET)
+            # Add sensors and update position
+            # Move forwad in time
+            if (simulation):
+                state = kin.forward(state, u_lqr, dt)
+            else:
+                turtle.set_velocity(u_lqr[0], u_lqr[1])
+                state, bumper = turtle.get_pos(OFFSET)
 
-            if (bumper == 1):
-                print("Bumper pressed")
+                if (bumper == 1):
+                    print("Bumper pressed")
+                    break
+
+            # Store the trajectory and estimated trajectory
+            trajectory = np.concatenate((trajectory, [state]), axis=0)
+
+            # Calculate the distance
+            distance_target = dist_points(state[0:2], desired_traj[index, 0:2])
+
+            # Validate if the target is the final target
+            if (index < (len(desired_traj) - 1)):
+                distance_target_next = dist_points(
+                    state[0:2], desired_traj[index + 1, 0:2])
+            else:
+                distance_target_next = distance_target
+
+            # Validate when to shift target
+            if (index < (len(desired_traj) - 1)):
+                if ((distance_target < dist_threshold) or
+                    (distance_target >= prev_distance) or
+                        (distance_target > distance_target_next)):
+                    prev_distance = distance_target_next
+                    index += 1
+                    print(index, " of ", len(desired_traj))
+
+            # Validate if goal is reached
+            elif ((distance_target < goal_dis) or
+                  (distance_target >= prev_distance)):
+                print("Completed run, offset: ", dist_points(
+                    state[0:2], desired_traj[-1, 0:2]))
                 break
 
-        # Store the trajectory and estimated trajectory
-        trajectory = np.concatenate((trajectory, [state]), axis=0)
+            # Update the distance error
+            else:
+                prev_distance = distance_target
 
-        # Calculate the distance
-        distance_target = dist_points(state[0:2], desired_traj[index, 0:2])
-
-        # Validate if the target is the final target
-        if (index < (len(desired_traj) - 1)):
-            distance_target_next = dist_points(
-                state[0:2], desired_traj[index + 1, 0:2])
-        else:
-            distance_target_next = distance_target
-
-        # Validate when to shift target
-        if (index < (len(desired_traj) - 1)):
-            if ((distance_target < dist_threshold) or
-                (distance_target >= prev_distance) or
-                    (distance_target > distance_target_next)):
-                prev_distance = distance_target_next
-                index += 1
-                print(index, " of ", len(desired_traj))
-
-        # Validate if goal is reached
-        elif ((distance_target < goal_dis) or
-              (distance_target >= prev_distance)):
-            print("Completed run, offset: ", dist_points(
-                state[0:2], desired_traj[-1, 0:2]))
-            break
-
-        # Update the distance error
-        else:
-            prev_distance = distance_target
+    except KeyboardInterrupt:
+        print("Interrupted")
 
     # Return the trajectory
     return trajectory
