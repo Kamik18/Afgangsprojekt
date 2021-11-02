b4cefdec6ec2cb3a7fa68f9dc3083343aff0a2a4
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2021-11-02 11:23:25 +0100

    change libraies and prepare for convert to yolo

--- Python/convertAnnotations.py
+++ Python/convertAnnotations.py
@@ -5,8 +5,8 @@ import csv
 import os
 
 # Variables
-train_path = './Car_Object_Detection/images/val'
-test_path = './Car_Object_Detection/images/test'
+train_path = './Alfa_Laval/images/train'
+test_path = './Alfa_Laval/images/test'
 
 def split_train_into_train_val():
     labels_path = './Car_Object_Detection/labels/val'
@@ -56,6 +56,25 @@ def BndBox2YoloLine(box, classList=[]):
     classIndex = classList.index(boxName)
     return classIndex, xcen, ycen, w, h
 
+def BndBox2YoloLine(box, classList=[]):
+    xmin = box[1]
+    xmax = box[2]
+    ymin = box[3]
+    ymax = box[4]
+    xcen = float((xmin + xmax)) / 2 / width
+    ycen = float((ymin + ymax)) / 2 / height
+
+    w = float((xmax - xmin)) / width
+    h = float((ymax - ymin)) / height
+
+    # PR387
+    boxName = box[0]
+    if boxName not in classList:
+        classList.append(boxName)
+
+    classIndex = classList.index(boxName)
+    return classIndex, xcen, ycen, w, h
+
     #plt.imshow(img)
 
 def create_labels():
@@ -78,34 +97,43 @@ def create_labels():
                 line_count += 1
         print(f'Processed {line_count} lines.')
 
-#create_labels()
+create_labels()
 
-alfa_path='./Alfa_Laval/training/'
+alfa_path='./Alfa_Laval/testing/'
 def print_img_with_annotation():
-    with open('./Alfa_Laval/Labels.csv') as csv_file:
+    with open('./Alfa_Laval/TestLabels.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
-            elif line_count == 1:
+            #else:
+            elif line_count == 12:
                 print(f'Values are {", ".join(row)}')
                 box = [0, float(row[2]), float(row[3]), float(row[4]), float(row[5])]
                 print(alfa_path + f'{row[0]}')
                 img = cv2.imread(alfa_path + f'{row[0]}')
-                img = img[..., ::-1]
-                b,g,r = cv2.split(img)       # get b,g,r
-                img = cv2.merge([r,g,b])     # switch it to rgb
+                #img = img[..., ::-1]
+                #b,g,r = cv2.split(img)       # get b,g,r
+                #img = cv2.merge([r,g,b])     # switch it to rgb
                 width, height = img.shape[1], img.shape[0]
                 print(width, height)
-                start_pont = (int(box[0]) - int(row[1]), int(box[2]) - int(box[3]))
-                print(start_pont)
+                x_start = int(box[1])
+                y_start = int(box[2])
+                x_end = int(box[1] + box[3])
+                y_end = int(box[2] + box[4])
+                start_point=(x_start, y_start)
+                end_point=(x_end, y_end)
+                print(start_point)
+                print(end_point)
+                img = cv2.rectangle(img, start_point, end_point, (255,0,0), 2)
+                #start_point = ((int(box[0]) - int(row[1])), (int(box[2]) - int(box[3])))
                 plt.imshow(img)
                 plt.show()
                 line_count += 1
             else:
-                pass
+                line_count += 1
                 #print(f'\tType: {row[0]}, path: {row[1]}, class: {row[2]}, x_min: {row[3]}, y_min: {row[4]}, x_max: {row[5]}, y_max: {row[8]}')
 
-print_img_with_annotation()
+#pr1int_img_with_annotation()
