d3733bc80c6744221a6b4e878ab7e106b65b80b0
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2021-11-03 10:37:08 +0100

    create val libriary

--- Python/convertAnnotations.py
+++ Python/convertAnnotations.py
@@ -5,8 +5,8 @@ import csv
 import os
 
 # Variables
-train_path = './Alfa_Laval/images/train'
-test_path = './Alfa_Laval/images/test'
+train_path = './Alfa_Laval/images/train/'
+test_path = './Alfa_Laval/images/test/'
 
 def split_train_into_train_val():
     labels_path = './Car_Object_Detection/labels/val'
@@ -29,24 +29,23 @@ def split_train_into_train_val():
         else:
             print("Not found")
 
-#print(test_path + '/vid_5_400.jpg')
-#img = cv2.imread(test_path + '/vid_5_400.jpg')
+img = cv2.imread(train_path + '//Alfa_Laval_Sensor_0.jpg')
 #img = img[..., ::-1]
-#b,g,r = cv2.split(img)       # get b,g,r
-#img = cv2.merge([r,g,b])     # switch it to rgb
-#width, height = img.shape[1], img.shape[0]
+b,g,r = cv2.split(img)       # get b,g,r
+img = cv2.merge([r,g,b])     # switch it to rgb
+width, height = img.shape[1], img.shape[0]
 #print(width, height)
+#plt.imshow(img)
+#plt.show()
 
-def BndBox2YoloLine(box, classList=[]):
-    xmin = box[1]
-    xmax = box[2]
-    ymin = box[3]
-    ymax = box[4]
-    xcen = float((xmin + xmax)) / 2 / width
-    ycen = float((ymin + ymax)) / 2 / height
-
-    w = float((xmax - xmin)) / width
-    h = float((ymax - ymin)) / height
+def CSV2YoloLine(box, classList=[]):
+    print(box)
+    xtopleft = box[1]
+    ytopleft = box[2]
+    w = float(box[3]) / width 
+    h = float(box[4]) / height
+    xcen = float(xtopleft + w / 2) / width
+    ycen = float(ytopleft + h / 2) / height
 
     # PR387
     boxName = box[0]
@@ -54,6 +53,7 @@ def BndBox2YoloLine(box, classList=[]):
         classList.append(boxName)
 
     classIndex = classList.index(boxName)
+    print(classIndex, xcen, ycen, w, h)
     return classIndex, xcen, ycen, w, h
 
 def BndBox2YoloLine(box, classList=[]):
@@ -78,8 +78,8 @@ def BndBox2YoloLine(box, classList=[]):
     #plt.imshow(img)
 
 def create_labels():
-    with open('./Car_Object_Detection/train_solution_bounding_boxes (1).csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    with open('./Alfa_Laval/TestLabels.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -87,19 +87,39 @@ def create_labels():
                 line_count += 1
             else:
                 #print(f'\tType: {row[0]}, path: {row[1]}, class: {row[2]}, x_min: {row[3]}, y_min: {row[4]}, x_max: {row[5]}, y_max: {row[8]}')
-                print(f'\tPath: {row[0]}, x_min: {row[1]}, y_min: {row[2]}, x_max: {row[3]}, y_max: {row[4]}')
-                box = [0, float(row[1]), float(row[3]), float(row[2]), float(row[4])]
-                label = BndBox2YoloLine(box, [0])
+                print(f'\tPath: {row[0]}, label: {row[1]}, x_topleft: {row[2]}, y_topleft: {row[3]}, x_width: {row[4]}, y_height: {row[5]}')
+                box = [0, float(row[2]), float(row[3]), float(row[4]), float(row[5])]
+                label = CSV2YoloLine(box, [0])
+                
+                #print(train_path + f'{row[0]}')
+                #img = cv2.imread(train_path + f'{row[0]}')
+                #img = img[..., ::-1]
+                #b,g,r = cv2.split(img)       # get b,g,r
+                #img = cv2.merge([r,g,b])     # switch it to rgb
+                #width, height = img.shape[1], img.shape[0]
+                #print(width, height)
+                #x_start = int(label[1] - (label[3] / 2 ))
+                #y_start = int(label[2] - (label[4] / 2 ))
+                #x_end = int(label[1] + (label[3] / 2 ))
+                #y_end = int(label[2] + (label[4] / 2 ))
+                #start_point=(x_start, y_start)
+                #end_point=(x_end, y_end)
+                #print(start_point)
+                #print(end_point)
+                #img = cv2.rectangle(img, start_point, end_point, (255,0,0), 2)
+                #plt.imshow(img)
+                #plt.show()
+
                 # Convert to each single file
-                with open(f'./Car_Object_Detection/labels/{row[0].replace("jpg", "txt")}', 'w') as f:
+                with open(f'./Alfa_Laval/labels/{row[0].replace("jpg", "txt")}', 'w') as f:
                     f.write("{} {} {} {} {}\n".format(label[0],label[1],label[2],label[3],label[4]))
 
                 line_count += 1
         print(f'Processed {line_count} lines.')
 
-create_labels()
+#create_labels()
 
-alfa_path='./Alfa_Laval/testing/'
+alfa_path='./Alfa_Laval/images/test/'
 def print_img_with_annotation():
     with open('./Alfa_Laval/TestLabels.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
@@ -137,3 +157,18 @@ def print_img_with_annotation():
                 #print(f'\tType: {row[0]}, path: {row[1]}, class: {row[2]}, x_min: {row[3]}, y_min: {row[4]}, x_max: {row[5]}, y_max: {row[8]}')
 
 #pr1int_img_with_annotation()
+path='./Alfa_Laval/images/test/'
+def resize_img():
+    testdir = os.listdir(path)
+    print(f'{path}{testdir[0]}')
+    img = cv2.imread(path + f'{testdir[0]}')
+
+    newimg = cv2.resize(img, (480,480))
+
+    
+    plt.imshow(img)
+    plt.show()
+    plt.imshow(newimg)
+    plt.show()
+
+resize_img()
