c423e0c5b9832041f56bbdc2a0aa8b9bae0fc10d
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2021-11-25 09:50:17 +0100

    move images and create a new object detection

--- Python/Yolov3.py
+++ Python/Yolov3.py
@@ -1,5 +1,9 @@
+# %%
 import torch
 import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import os
 
 
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
@@ -9,6 +13,33 @@ labels = torch.randint(1, 91, (4, 11))
 #images = list(image for image in images)
 print(images)
 
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'data/alfalaval/images'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'test']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
+class_names = image_datasets['train'].classes
+print(dataset_sizes)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# %%
 """
 targets = []
 for i in range(len(images)):
