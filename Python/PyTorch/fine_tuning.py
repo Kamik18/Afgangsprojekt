ba99dbd7b2af1f3f034058fbdac4aff70a871f5f
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2021-11-26 08:10:36 +0100

    move stuff around

---
+++ Python/PyTorch/fine_tuning.py
@@ -0,0 +1,418 @@
+# %%
+# Imports
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import cv2
+import pandas as pd
+from pathlib import Path
+import random
+from sklearn.model_selection import train_test_split
+import xml.etree.ElementTree as ET
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+
+# %%
+# Top level data directory. Here we assume the format of the directory conforms to the ImageFolder structure
+data_dir = "../data/alfalaval"
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "resnet"
+
+# Number of classes in the dataset
+num_classes = 1
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 8
+
+# Number of epochs to train for
+num_epochs = 15
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
+
+
+#%%
+# Model for traning
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+#%%
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+# Initialize the model for this run
+model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+# Print the model we just instantiated
+print(model_ft)
+
+
+#%%
+
+images_path = Path(f'{data_dir}/images')
+anno_path = Path(f'{data_dir}/annotations')
+
+# Create methods for loading in images and annotations
+def filelist(root, file_type):
+    """Returns a fully-qualified list of filenames under root directory"""
+    return [os.path.join(directory_path, f) for directory_path, directory_name, 
+            files in os.walk(root) for f in files if f.endswith(file_type)]
+
+def generate_train_df (anno_path):
+    annotations = filelist(anno_path, '.xml')
+    anno_list = []
+    for anno_path in annotations:
+        root = ET.parse(anno_path).getroot()
+        anno = {}
+        anno['filename'] = Path(str(images_path) + '/'+ root.find("./filename").text)
+        anno['width'] = root.find("./size/width").text
+        anno['height'] = root.find("./size/height").text
+        anno['class'] = root.find("./object/name").text
+        anno['xmin'] = int(root.find("./object/bndbox/xmin").text)
+        anno['ymin'] = int(root.find("./object/bndbox/ymin").text)
+        anno['xmax'] = int(root.find("./object/bndbox/xmax").text)
+        anno['ymax'] = int(root.find("./object/bndbox/ymax").text)
+        anno_list.append(anno)
+    return pd.DataFrame(anno_list)
+
+# Generate train dataframes
+df_train = generate_train_df(anno_path)
+# Label encode target
+class_dict = {'alfa': 0}
+df_train['class'] = df_train['class'].apply(lambda x:  class_dict[x])
+
+# Print dataframe shape
+print(df_train.shape)
+df_train.head()
+
+# %%
+# Define methods for resizing and bounding boxes
+def read_image(path):
+    return cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB)
+
+def create_mask(bb, x):
+    """Creates a mask for the bounding box of same shape as image"""
+    rows,cols,*_ = x.shape
+    Y = np.zeros((rows, cols))
+    bb = bb.astype(np.int)
+    Y[bb[0]:bb[2], bb[1]:bb[3]] = 1.
+    return Y
+
+def mask_to_bb(Y):
+    """Convert mask Y to a bounding box, assumes 0 as background nonzero object"""
+    cols, rows = np.nonzero(Y)
+    if len(cols)==0: 
+        return np.zeros(4, dtype=np.float32)
+    top_row = np.min(rows)
+    left_col = np.min(cols)
+    bottom_row = np.max(rows)
+    right_col = np.max(cols)
+    return np.array([left_col, top_row, right_col, bottom_row], dtype=np.float32)
+
+def create_bb_array(x):
+    """Generates bounding box array from a train_df row"""
+    return np.array([x[5],x[4],x[7],x[6]])
+
+def resize_image_bb(read_path,write_path,bb,sz):
+    """Resize an image and its bounding box and write image to new path"""
+    im = read_image(read_path)
+    im_resized = cv2.resize(im, (int(1.49*sz), sz))
+    Y_resized = cv2.resize(create_mask(bb, im), (int(1.49*sz), sz))
+    new_path = str(write_path/read_path.parts[-1])
+    cv2.imwrite(new_path, cv2.cvtColor(im_resized, cv2.COLOR_RGB2BGR))
+    return new_path, mask_to_bb(Y_resized)
+# %%
+new_paths = []
+new_bbs = []
+train_path_resized = Path(f'{data_dir}/images_resized')
+for index, row in df_train.iterrows():
+    new_path,new_bb = resize_image_bb(row['filename'], train_path_resized, create_bb_array(row.values),300)
+    new_paths.append(new_path)
+    new_bbs.append(new_bb)
+df_train['new_path'] = new_paths
+df_train['new_bb'] = new_bbs
+#%%
+# Data Augmentation - define methods
+# modified from fast.ai
+def crop(im, r, c, target_r, target_c): 
+    return im[r:r+target_r, c:c+target_c]
+
+# random crop to the original size
+def random_crop(x, r_pix=8):
+    """ Returns a random crop"""
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2*rand_r*r_pix).astype(int)
+    start_c = np.floor(2*rand_c*c_pix).astype(int)
+    return crop(x, start_r, start_c, r-2*r_pix, c-2*c_pix)
+
+def center_crop(x, r_pix=8):
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    return crop(x, r_pix, c_pix, r-2*r_pix, c-2*c_pix)
+
+def rotate_cv(im, deg, y=False, mode=cv2.BORDER_REFLECT, interpolation=cv2.INTER_AREA):
+    """ Rotates an image by deg degrees"""
+    r,c,*_ = im.shape
+    M = cv2.getRotationMatrix2D((c/2,r/2),deg,1)
+    if y:
+        return cv2.warpAffine(im, M,(c,r), borderMode=cv2.BORDER_CONSTANT)
+    return cv2.warpAffine(im,M,(c,r), borderMode=mode, flags=cv2.WARP_FILL_OUTLIERS+interpolation)
+
+def random_cropXY(x, Y, r_pix=8):
+    """ Returns a random crop"""
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2*rand_r*r_pix).astype(int)
+    start_c = np.floor(2*rand_c*c_pix).astype(int)
+    xx = crop(x, start_r, start_c, r-2*r_pix, c-2*c_pix)
+    YY = crop(Y, start_r, start_c, r-2*r_pix, c-2*c_pix)
+    return xx, YY
+
+def transformsXY(path, bb, transforms):
+    x = cv2.imread(str(path)).astype(np.float32)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)/255
+    Y = create_mask(bb, x)
+    if transforms:
+        rdeg = (np.random.random()-.50)*20
+        x = rotate_cv(x, rdeg)
+        Y = rotate_cv(Y, rdeg, y=True)
+        if np.random.random() > 0.5: 
+            x = np.fliplr(x).copy()
+            Y = np.fliplr(Y).copy()
+        x, Y = random_cropXY(x, Y)
+    else:
+        x, Y = center_crop(x), center_crop(Y)
+    return x, mask_to_bb(Y)
+
+def create_corner_rect(bb, color='red'):
+    bb = np.array(bb, dtype=np.float32)
+    return plt.Rectangle((bb[1], bb[0]), bb[3]-bb[1], bb[2]-bb[0], color=color,
+                         fill=False, lw=3)
+
+def show_corner_bb(im, bb):
+    plt.imshow(im)
+    plt.gca().add_patch(create_corner_rect(bb))
+#%%
+# Train-valid split
+df_train = df_train.reset_index()
+X = df_train[['new_path', 'new_bb']]
+Y = df_train['class']
+X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+# %%
+# Define datset methods
+def normalize(im):
+    """Normalizes images with Imagenet stats."""
+    imagenet_stats = np.array([[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]])
+    return (im - imagenet_stats[0])/imagenet_stats[1]
+
+class RoadDataset(Dataset):
+    def __init__(self, paths, bb, y, transforms=False):
+        self.transforms = transforms
+        self.paths = paths.values
+        self.bb = bb.values
+        self.y = y.values
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        y_class = self.y[idx]
+        x, y_bb = transformsXY(path, self.bb[idx], self.transforms)
+        x = normalize(x)
+        x = np.rollaxis(x, 2)
+        return x, y_class, y_bb
+
+# %%
+#Setup dataset
+train_ds = RoadDataset(X_train['new_path'],X_train['new_bb'] ,y_train, transforms=True)
+valid_ds = RoadDataset(X_val['new_path'],X_val['new_bb'],y_val)
+batch_size = 8
+train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+valid_dl = DataLoader(valid_ds, batch_size=batch_size)
+
+
+#%%
+im = cv2.imread(str(df_train.values[68][9]))
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+fig = plt.figure(figsize=(10, 7))
+rows = 1
+columns = 2
+fig.add_subplot(rows, columns, 1)
+#original
+plt.title("Original")
+show_corner_bb(im, df_train.values[68][9])
+fig.add_subplot(rows, columns, 2)
+# after transformation
+plt.title("After Transform")
+im, bb = transformsXY(str(df_train.values[68][8]),df_train.values[68][9],True )
+show_corner_bb(im, bb)
